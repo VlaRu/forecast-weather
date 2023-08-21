ac89c8f1f380bbb2a0cfb5fc5d493ac28e21a1c0
Vladyslava Nikitchenko <dn210690nvs@gmail.com>
2023-08-21 11:12:50 +0200

    feat: add toggle highlights for converTemp.js

--- converTemp.js
+++ converTemp.js
@@ -7,13 +7,17 @@ export function convertHandlers(initialTemp) {
   fahrenheit.onclick = (e) => {
     e.preventDefault()
     degrees.innerHTML  = Math.floor((initialTemp * 9) / 5 + 32);
+    celsius.classList.remove("highlights");
+    fahrenheit.classList.add("highlights");
   }
   
   //convert in celsius
   celsius.onclick = (e) => {
     e.preventDefault()
     degrees.innerHTML = initialTemp;
+    fahrenheit.classList.remove("highlights");
+    celsius.classList.add("highlights");
   }
 }
 
-/*if you celsius.onclick class="c" will add class active, and if you fahrenheit.onclick class="f" will add class active*/
+/*i want to add in my code condition ,if you celsius.onclick class="c" will add class highlights, and if you fahrenheit.onclick class="f" will add class highlights*/
